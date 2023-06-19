9ad4194e84b5739b03d92225025a2525ce0ae534
Hog <6062207@gmail.com>
2023-06-19 19:37:11 +0200

    feat:zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
-const compress = async () => {
-    // Write your code here 
+import { createGzip } from 'zlib';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import  path  from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+const inputFile = path.join(__dirname,'files','fileToCompress.txt');
+const outputFile = path.join(__dirname,'files','archive.gz')
+
+export const compress = async () => {
+    const readStream = fs.createReadStream(inputFile);
+    const writeStream = fs.createWriteStream(outputFile);
+    const gzipStream = createGzip();
+  
+    readStream.pipe(gzipStream).pipe(writeStream);
 };
 
-await compress();
+compress();
